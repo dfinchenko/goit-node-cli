5ac36a8a6a5b5724b9e3e412c0eaffcb084aff3b
dfinchenko <dfinchenko@gmail.com>
2025-03-13 23:57:14 +0100

    home work

--- contacts.js
+++ contacts.js
@@ -1,6 +1,6 @@
 import fs from 'node:fs/promises';
+import path from 'node:path';
 import { nanoid } from 'nanoid';
-import {path} from 'node:path';
 
 const contactsPath = path.join('db', 'contacts.json');
 
